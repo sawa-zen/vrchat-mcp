9c3119ca97b9424ef36efc9db9524702fa9f27a0
sawa-zen <sawadasuiren@gmail.com>
2025-03-16 22:46:44 +0900

    fix: friends description

--- src/tools/friends.ts
+++ src/tools/friends.ts
@@ -2,8 +2,29 @@ import { McpServer } from "@modelcontextprotocol/sdk/server/mcp"
 import { VRChatClient } from "../VRChatClient"
 
 export const createFriendsTools = (server: McpServer, vrchatClient: VRChatClient) => {
-  server.tool("vrchat_get_friends",
-    "Retrieve a list of VRChat friend information",
+  server.tool(
+    "vrchat_get_friends", // Name
+    `Retrieve a list of VRChat friend information. The following information can be retrieved:
+    - "bio"
+    - "bioLinks"
+    - "currentAvatarImageUrl"
+    - "currentAvatarThumbnailImageUrl"
+    - "currentAvatarTags"
+    - "developerType"
+    - "displayName"
+    - "fallbackAvatar"
+    - "id"
+    - "isFriend"
+    - "last_platform"
+    - "last_login"
+    - "profilePicOverride"
+    - "pronouns"
+    - "status"
+    - "statusDescription"
+    - "tags"
+    - "userIcon"
+    - "location"
+    - "friendKey"`, // Description
     {}, // No parameters
     async () => {
       try {
